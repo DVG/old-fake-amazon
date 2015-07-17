0607c15e1c36fc7c04855b145f588881ecb2f74d
Bradley Temple <bradley.temple@jpmchase.net>
2015-07-17 12:39:07 -0400

    maximize window

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -4,4 +4,5 @@ Before do
   ShippingMethod.create identifier: ShippingMethod::PRIME_TWO_DAY, name: "FREE Prime 2-Day Shipping", price_per_item: 0
   ShippingMethod.create identifier: ShippingMethod::PRIME_ONE_DAY, name: "$3.99 Prime Overnight Shipping", price_per_item: 3.99
   ShippingMethod.create identifier: ShippingMethod::PRIME_NO_RUSH, name: "FREE No-Rush Shipping", price_per_item: 0
+  page.driver.browser.manage.window.maximize
 end
